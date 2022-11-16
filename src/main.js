9614b0302e04d2e43d55414903ff8d780ae6835b
Kate1513 <kate_1513@hotmail.com>
2022-11-15 20:36:03 -0500

    v2 - HU1 - Funciones para renderizar las paginas con sus listener

--- src/main.js
+++ src/main.js
@@ -2,15 +2,27 @@ import { filterData } from './data.js';
 import { sortData } from './data.js';
 import data from './data/ghibli/ghibli.js';
 
+let nodesToBeAppend = []
 const dataFilms = data.films
+const dataCharactersByFilm = []
+dataFilms.forEach(film => dataCharactersByFilm.push(film.people))
 
-let allMovies = []
+const allCharacters = dataCharactersByFilm.flat()
+
+const linkPeliculas = document.querySelector('.peliculas')
+const linkPersonajes = document.querySelector('.personajes')
+const linkEstudio = document.querySelector('.estudio')
 const containerNode = document.querySelector('.container')
 const sortSelector = document.querySelector('#sortSelector')
 const search = document.querySelector('.src')
 const menu = document.querySelector('.menu')
 const menuIcon = document.querySelector('.menu-icon')
 const mediaQueryDesktop = window.matchMedia("(min-width: 768px)");
+
+// Estado inicial de la pagina
+renderMovies(dataFilms)
+
+// Listeners para aplicar funcionalidad
 
 mediaQueryDesktop.addEventListener ('change', () => {
     if (!menu.classList.contains('menu-inactive')) {
@@ -21,6 +33,64 @@ mediaQueryDesktop.addEventListener ('change', () => {
 menuIcon.addEventListener('click', () => {
     menu.classList.toggle('menu-inactive')
 })
+
+linkPeliculas.addEventListener('click', () => {
+    clearNodes(containerNode)
+    // Hace falta cambiar page-header
+    renderMovies(dataFilms)
+})
+
+linkPersonajes.addEventListener('click', () => {
+    clearNodes(containerNode)
+    // Hace falta cambiar page-header
+    renderCharacters(allCharacters)
+})
+
+linkEstudio.addEventListener('click', () => {
+    clearNodes(containerNode)
+    // Hace falta cambiar page-header
+    // renderEstudio()
+})
+
+search.addEventListener('change', (event) => {
+   clearNodes(containerNode)  
+   if (event.target.value != '') {    
+   let filter = filterData(dataFilms, event.target.value)
+   renderMovies(filter)
+   } else {
+    clearNodes(containerNode)
+    renderMovies(dataFilms)
+    containerNode.classList.add('container')
+   }
+})
+
+sortSelector.addEventListener('change', (event) => {
+    clearNodes(containerNode)
+    if (event.target.value === 'A-Z'){
+        renderMovies(sortData(data.films, 'A-Z'))   
+    }
+    if (event.target.value === 'Z-A'){
+        renderMovies(sortData(data.films, 'A-Z', 'DES'))   
+    }
+    if (event.target.value === 'Year1-9') {       
+        renderMovies(sortData(data.films, 'Year')) 
+    }
+    if (event.target.value === 'Year9-1') {       
+        renderMovies(sortData(data.films, 'Year', 'DES'))
+    }
+    if (event.target.value === 'Ranking') {       
+        renderMovies(sortData(data.films, 'Ranking', 'DES'))
+    }
+})
+
+// Declaracion de las funciones
+
+function clearNodes (parentNode){
+    nodesToBeAppend = []
+    while (parentNode.firstChild){
+        parentNode.removeChild(parentNode.firstChild)
+    }
+}
 
 function renderMovies (films) {
     if (films.length > 0) {
@@ -37,63 +107,69 @@ function renderMovies (films) {
         const ranking = document.createElement("h2")
         ranking.textContent = `Ranking: ${item.rt_score}`
     
-        const container = document.createElement("div")
-        container.append(image, name, date, ranking)
-        allMovies.push(container)
+        const content = document.createElement("div")
+        content.append(image, name, date, ranking)
+        nodesToBeAppend.push(content)
+        containerNode.append(...nodesToBeAppend)
         })
     } else {
-        const a = document.createElement("h3")
-        a.textContent = 'No se encuentran resultados de la busqueda, por favor intente de nuevo.'
-        const container = document.createElement("h3")
-        container.append(a)
-        allMovies.push(container)
+        const notFoundInfo = document.createElement("h3")
+        notFoundInfo.textContent = 'No se encuentran resultados de la busqueda, por favor intente de nuevo.'
+        containerNode.classList.remove('container')
+        containerNode.append(notFoundInfo)        
     }
 }
 
-function clearNodes (parentNode){
-    while (parentNode.firstChild){
-        parentNode.removeChild(parentNode.firstChild)
+function renderCharacters (characters) {
+    if (characters.length > 0) {
+        characters.forEach((item) => {
+        const image = document.createElement("img")
+        image.src = item.img
+        
+        const name = document.createElement("h2")
+        name.textContent = item.name
+    
+        const age = document.createElement("h2")
+        age.textContent = `Edad: ${item.age}`
+    
+        const content = document.createElement("div")
+        content.append(image, name, age)
+        nodesToBeAppend.push(content)
+        containerNode.append(...nodesToBeAppend)
+        })
+    } else {
+        const notFoundInfo = document.createElement("h3")
+        notFoundInfo.textContent = 'No se encuentran resultados de la busqueda, por favor intente de nuevo.'
+        containerNode.classList.remove('container')
+        containerNode.append(notFoundInfo) 
     }
 }
 
-search.addEventListener('change', (event) => {
-   allMovies = []
-   clearNodes(containerNode)  
-   if (event.target.value != '') {    
-   let filter = filterData(dataFilms, event.target.value)
-   renderMovies(filter)
-   containerNode.append(...allMovies)
-   } else {
-    renderMovies(dataFilms)
-    clearNodes(containerNode)
-    containerNode.append(...allMovies) 
-   }
-})
+// function renderCharacters (name) {
+//     if (name.length > 0) {
+//         name.forEach((item) => {
 
-sortSelector.addEventListener('change', (event) => {
-    allMovies = []
-    clearNodes(containerNode)
-    if (event.target.value === 'A-Z'){
-        renderMovies(sortData(data.films, 'A-Z'))
-        containerNode.append(...allMovies)     
-    }
-    if (event.target.value === 'Z-A'){
-        renderMovies(sortData(data.films, 'A-Z', 'DES'))
-        containerNode.append(...allMovies)     
-    }
-    if (event.target.value === 'Year1-9') {       
-        renderMovies(sortData(data.films, 'Year'))
-        containerNode.append(...allMovies) 
-    }
-    if (event.target.value === 'Year9-1') {       
-        renderMovies(sortData(data.films, 'Year', 'DES'))
-        containerNode.append(...allMovies) 
-    }
-    if (event.target.value === 'Ranking') {       
-        renderMovies(sortData(data.films, 'Ranking', 'DES'))
-        containerNode.append(...allMovies) 
-    }
-})
- 
-renderMovies(dataFilms)
-containerNode.append(...allMovies)
+//         const image = document.createElement("img")
+//         image.src = item.img
+        
+//         const name = document.createElement("h2")
+//         name.textContent = item.name
+    
+//         const date = document.createElement("h2")
+//         date.textContent = `Fecha: ${item.release_date}`
+
+//         const ranking = document.createElement("h2")
+//         ranking.textContent = `Ranking: ${item.rt_score}`
+    
+//         const container = document.createElement("div")
+//         container.append(image, name, date, ranking)
+//         allMovies.push(container)
+//         })
+//     } else {
+//         const a = document.createElement("h3")
+//         a.textContent = 'No se encuentran resultados de la busqueda, por favor intente de nuevo.'
+//         const container = document.createElement("h3")
+//         container.append(a)
+//         allMovies.push(container)
+//     }
+// }
